6a459d0426b154d182ded4a822ba45769179af8b
grinenkiv <59543157+grinenkiv@users.noreply.github.com>
2020-01-05 19:44:17 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,8 @@
 ## Caching the Inverse of a Matrix:
 ## Pair of functions that are used to create a special object that 
 ## stores a matrix and caches its inverse
-
 ## The first function in the file, makeCacheMatrix() 
 ## creates an R object that stores a matrix and its inverse
-
 makeCacheMatrix <- function(x = matrix()) {
       inv <- NULL
       set <- function(y) {
@@ -18,12 +16,9 @@ makeCacheMatrix <- function(x = matrix()) {
            setinvers = setinvers,
            getinvers = getinvers)
 }
-
-
 ## The second function, cacheSolve() requires an argument that is returned by 
 ## makeCacheMatrix() in order to retrieve the mean from the cached value 
 ## that is stored in the makeCacheMatrix() object's environment.
-
 cacheSolve <- function(x, ...) {
       inv <- x$getinvers()
       if(!is.null(inv)) {
